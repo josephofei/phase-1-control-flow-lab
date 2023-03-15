8f389e50a84b17e6b67d855d6deb8ab27a4a8142
Joseph Ofei <josephofei24@gmail.com>
2023-03-15 06:49:29 -0400

    addss complete control-flow-lab.

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(sample) {
   // Write your code here!
+
+  if (sample <= 400) {
+    return `This one is on me!`
+  }
+  else if (sample > 400 && sample < 2000) {
+    return `That will be twenty bucks.`
+  }
+  else if (sample > 2000 && sample < 2500) {
+    return `I will gladly take your thirty bucks.`
+  }
+  else if (sample > 2500) {
+    return 'No can do.';
+  }
+
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city) {
   // Write your code here!
+
+  const cityName = city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
+
+  return cityName;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip) {
   // Write your code here!
+  switch (tip) {
+    case 'generous':
+      return `Thank you so much.`;
+      break;
+    case 'not as generous':
+      return `Thank you.`;
+      break;
+    default:
+      return `Bye.`
+
+  }
+
+
+
 }
